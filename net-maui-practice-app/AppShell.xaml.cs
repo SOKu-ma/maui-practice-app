8d460e0269617322d42932ea0a5dd7833f5583ae
otashuuhei <uverrevu812@gmail.com>
2023-06-11 23:50:44 +0900

    Initial Commit

---
+++ net-maui-practice-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace net_maui_practice_app;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ net-maui-practice-app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace net_maui_practice_app;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
